c6e0ef8282d76661ca81d1d994ff6654da371205
Arjun <arjunjain08@gmail.com>
2015-01-30 15:17:27 +0530

    add performce shema as ignored database

--- config.php
+++ config.php
@@ -5,4 +5,4 @@ define('DB_PASS','');					 // Database Password
 define('DB_DIR','/home/geekpoint/backup/');  // Full Path to directory when backup file store. Should have write permission.
 define('DB_KEEP',2);   						 // Number of database backup to store
 define('DS', DIRECTORY_SEPARATOR);
-$database_to_ignore=array('information_schema','mysql');
+$database_to_ignore=array('information_schema','mysql','performance_schema');
